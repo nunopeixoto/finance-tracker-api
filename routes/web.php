bd1c2c728bc50b7177e0a9db91989917c44dd9c8
Peixoto <nuno_peixoto1997@hotmail.com>
2021-08-05 22:53:18 +0100

    Home response is now in JSON format

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Welcome to Finance Tracker API.';
+    return response()->json('Welcome to Finance Tracker API.');
 });
