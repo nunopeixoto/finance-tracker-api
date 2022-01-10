7c123ccdbca5cfc248b60a61161ef00e7a75fa6a
Peixoto <nuno_peixoto1997@hotmail.com>
2022-01-10 23:19:42 +0000

    Make sub category extend category

--- app/Models/ExpenseSubCategory.php
+++ app/Models/ExpenseSubCategory.php
@@ -2,33 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
-
-class ExpenseSubCategory extends Model
+class ExpenseSubCategory extends ExpenseCategory
 {
-    use HasFactory;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'user_id',
-        'description'
-    ];
-
-    public $timestamps = false;
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function scopeQueryUser(Builder $query, int $userId) : Builder
-    {
-        return $query->where('user_id', $userId);
-    }
 }
