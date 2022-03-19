57138eb1e6ae0a7f1d28c09cf785c02a6b373e9e
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-19 18:00:45 +0000

    AuthControllerTest formatting

--- tests/Feature/Http/Controllers/AuthControllerTest.php
+++ tests/Feature/Http/Controllers/AuthControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Http\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
@@ -11,11 +10,7 @@ use Laravel\Sanctum\Sanctum;
 class AuthControllerTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+
     public function test_register() : void
     {
         // OK
